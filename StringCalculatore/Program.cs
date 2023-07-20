bbae6f84b493cdfec8d5e959bff0b7d63cf87b31
robwiega <robwiega@WKWDE5001221>
2023-07-20 23:10:37 +0200

    adding + functionallity

--- StringCalculatore/Program.cs
+++ StringCalculatore/Program.cs
@@ -2,6 +2,32 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string input = "9 + 10 + 1";
+
+        // Creating List to store the clean numbers
+        List<int> numbers= new List<int>();
+
+        // I need to split the string into different parts, where the operators are.
+        // However I need to take the operator into account.
+
+        string[] values = input.Split('+');
+
+        // check length of array and iterate as often through the array
+        foreach (string value in values)
+        {
+            int number = int.Parse(value.Trim());
+            numbers.Add(number);
+        }
+
+        // Adding all values together
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        // Printing the result
+        Console.WriteLine(sum);
+
     }
 }
